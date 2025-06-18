77208de320da4f199e9006fd7ec60c35120b08b7
unknown <neverhappystudios@gmail.com>
2025-06-18 16:35:45 -0400

    Removed project specific content. "

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,21 +1,21 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+// class SplashScreen extends StatelessWidget {
+//   const SplashScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
+//   @override
+//   Widget build(BuildContext context) {
+//     final screenWidth = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: Container(
-        color: Colors.black,
-        child: Center(
-          widthFactor: screenWidth * .7,
-          child: SvgPicture.asset('assets/NeverHappyLogoName.svg'),
-        ),
-      ),
-    );
-  }
-}
+//     return Scaffold(
+//       body: Container(
+//         color: Colors.black,
+//         child: Center(
+//           widthFactor: screenWidth * .7,
+//           child: SvgPicture.asset('assets/NeverHappyLogoName.svg'),
+//         ),
+//       ),
+//     );
+//   }
+// }
