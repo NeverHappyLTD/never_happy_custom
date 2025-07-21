108938e0de005ed1622a27e6f57a8a88c4f37777
NeverHappyLTD <brian@neverhappy.net>
2025-07-21 17:26:00 -0400

    added barrel file

---
+++ lib/never_happy_custom.dart
@@ -0,0 +1,11 @@
+library never_happy_custom;
+
+// Widgets
+export 'widgets/outlined_text/outlined_auto_size_text.dart';
+export 'widgets/outlined_text/outlined_text.dart';
+export 'widgets/show_legal.dart';
+
+// Theme & Styling
+export 'widgets/pink_background.dart';
+export 'widgets/styling.dart';
+export 'package:never_happy_custom/widgets/base_scaffold.dart';
